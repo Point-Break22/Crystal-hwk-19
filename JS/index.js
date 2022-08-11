27f16d1c60bd277475f534255cb1e555172d9504
Point-Break22 <crystalsjgayle@gmail.com>
2022-08-11 18:32:34 -0400

    replaced after testing

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,13 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
-const bgColour = document.querySelector('.bg-colour')
-let load = 0
-let int = setInterval(blurring, 10)
-function blurring() {
-  load++
-  if (load > 99) {
-    clearInterval(int)
-  }
-  loadText.innerText = `${load}%`
-  loadText.style.opacity = scale(load, 0, 100, 1, 0)
-  bgColour.style.opacity = scale(load, 0, 100, 1, 0)
-}
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
+$( "li" ).hover(
+    function() {
+      $( this ).append( $( "<span> ***</span>" ) );
+    }, function() {
+      $( this ).find( "span" ).last().remove();
+    }
+  );
+   
+  $( "li.fade" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+  });s
+  
